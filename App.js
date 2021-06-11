21d3768f43f4e9101a29b12da6b4b930239bdfdd
Fuxuan Gong <fuxuan.gong@nextgentel.com>
2021-06-11 11:56:40 +0200

    move out settings to seperate file

--- App.js
+++ App.js
@@ -7,19 +7,7 @@ app.use(express.json()); //Used to parse JSON bodies
 
 require("dotenv").config();
 
-//see the settings doc from https://github.com/appfeel/node-pushnotifications
-//to support more destinations like Android / Web etc.
-const settings = {
-  apn: {
-    token: {
-      key: process.env.APN_KEY, 
-      keyId: process.env.APN_KEY_ID, 
-      teamId: process.env.APN_TERM_ID 
-    },
-    production: false // true for APN production environment, false for APN sandbox environment,
-  },
-  isAlwaysUseFCM: false // true all messages will be sent through node-gcm (which actually uses FCM)
-};
+const settings = require("./Settings");
 
 const PushNotifications = require("node-pushnotifications");
 const push = new PushNotifications(settings);
