69cead5dd35fcba51e5d35c90748689374c28380
Fuxuan Gong <fuxuan.gong@nextgentel.com>
2021-06-11 13:24:04 +0200

    add tests

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
+const { body, validationResult } = require('express-validator');
 const express = require("express");
 const app = express();
 app.set("port", process.env.PORT);
 app.use(express.urlencoded()); //Parse URL-encoded bodies
 app.use(express.json()); //Used to parse JSON bodies
-
 
 require("dotenv").config();
 
@@ -14,25 +14,36 @@ const push = new PushNotifications(settings);
 
 //Send push message as req.body.data to a list of destinations specified by req.body.registrationIds
 //See the req.body.data format from https://github.com/appfeel/node-pushnotifications
-//Test the path like this: 
+//Test the path like this:
 //    curl -v http://localhost:8000/send-message -d '{"sender":'testing", "registrationIds":["b2d0e95bfce054a3d220da12280cfdbb15274a1a9e5a02e5618c72b6ee32d0e2b"], "data":{"topic":"com.who.test.voip","mydata":"12341234","aps":{"alert":"hello"}}}' -H "Content-Type: application/json"
-app.post("/send-message", function (req, res) {
-  console.log("Client asks to send-message: ", req.body);
-  push
-    .send(req.body.registrationIds, req.body.data)
-    .then(results => {
+app.post(
+  "/send-message",
+  body("sender").notEmpty(),
+  body("registrationIds").isArray({ min: 1 }),
+  body("data").isObject(),
+   (req, res)=> {
+    console.log("Client asks to send-message: ", req.body);
+
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.error("The request has validation errors: ", errors);
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    push
+      .send(req.body.registrationIds, req.body.data)
+      .then(results => {
         console.log("send-message results: ", results);
         res.json(results);
-    })
-    .catch(err => {
-      console.log("send-message failed: ", err);
-      res.status(400).json(err);
-    });
-});
+      })
+      .catch(err => {
+        console.error("send-message failed: ", err);
+        res.status(400).json(err);
+      });
+  });
 
-app.listen(process.env.PORT, () => {
-  console.log(`server running on port: ${process.env.PORT}`);
-});
+module.exports = app;
 
 // ------todo: log send-message req/resp to mysql database
 // ----- npm add mysql2
